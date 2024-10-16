74a0a965b5e8b7546ef55e6a81bad01cbf3c6df5
max2556 <46759632+max2556@users.noreply.github.com>
2024-10-17 01:44:18 +0300

    smooth scrolling numbers

--- islands/Timer.tsx
+++ islands/Timer.tsx
@@ -1,18 +1,48 @@
 import { useComputed, useSignal } from "@preact/signals";
-import { useEffect } from "preact/hooks";
+import { useEffect, useRef } from "preact/hooks";
 import { difference } from "$std/datetime/mod.ts";
 
 interface Props {
   from: number; // Date.getTime()
   to: number; // Date.getTime()
+
+  class?: string;
+}
+
+function NumbersColumn(props: { value: number | string }) {
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const elNumbers = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!elNumbers.current || !container.current) return;
+    const el = elNumbers.current.querySelector(`.num_${props.value}`);
+    if (!el) return;
+    container.current?.scrollTo({
+      top: el.getBoundingClientRect().top - elNumbers.current.getBoundingClientRect().top,
+      behavior: "smooth",
+    });
+  }, [props.value]);
+
+  return (
+    <div class="relative h-fit overflow-hidden" ref={container}>
+      <p class="invisible">{props.value}</p>
+      <div class="absolute top-0" ref={elNumbers}>
+        {numbers.map((number) => <p class={`num_${number}`}>{number}</p>)}
+      </div>
+    </div>
+  );
 }
 
 function TimeCell(props: { value: string }) {
   const value = props.value.padStart(2, "0");
 
   return (
-    <div class="tracking-wide">
-      <p>{value}</p>
+    <div class="flex tracking-wide">
+      {/* <p>{value[0]}</p> */}
+      {/* <p>{value[1]}</p> */}
+      <NumbersColumn value={value[0]}></NumbersColumn>
+      <NumbersColumn value={value[1]}></NumbersColumn>
     </div>
   );
 }
@@ -47,7 +77,16 @@ export default function Timer(props: Props) {
   });
 
   return (
-    <div class="flex items-center justify-center gap-x-2 py-6 select-none">
+    <div
+      class={"flex items-center justify-center gap-x-2 py-6 select-none " +
+        (props.class ?? "")}
+    >
+      {
+        /* <NumbersColumn
+        value={delta.value.seconds.toString().padStart(2, "0")[1]}
+      /> */
+      }
+
       <TimeCell value={delta.value.days.toString()} />
       <p>:</p>
       <TimeCell value={delta.value.hours.toString()} />
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,31 +1,22 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
 import Timer from "../islands/Timer.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
-
   const to = new Date();
   to.setUTCDate(19);
   to.setUTCHours(18, 0, 0, 0);
 
   return (
-    <div class="px-4 py-8 mx-auto bg-gradient-to-b from-[#60ab2c] from-20% to-[#9c643f] to-40% h-screen">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center bg-none">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-        <Timer from={new Date().getTime()} to={to.getTime()}></Timer>
+    // <div class="px-4 py-8 mx-auto bg-gradient-to-b from-[#60ab2c] from-20% to-[#9c643f] to-40% h-screen">
+    <div class="mx-auto h-screen bg-[url('/bg.jpg')] bg-cover">
+      <div class="backdrop-blur-3xl h-full w-full p-8">
+        <div class="max-w-screen-md mx-auto h-full flex flex-col items-center justify-center bg-none">
+          <Timer
+            from={new Date().getTime()}
+            to={to.getTime()}
+            class="text-[3rem]"
+          >
+          </Timer>
+        </div>
       </div>
     </div>
   );
