ffd5df61732600fa1bf139854e3625b6cbc63d2c
max2556 <46759632+max2556@users.noreply.github.com>
2024-10-21 23:56:05 +0300

    .env support

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,18 @@
 import Timer from "../islands/Timer.tsx";
 
 export default function Home() {
-  const to = new Date();
-  to.setUTCDate(19);
-  to.setUTCHours(18, 0, 0, 0);
+  const to = new Date(Deno.env.get("START_DATETIME") as string);
 
   return (
-    // <div class="px-4 py-8 mx-auto bg-gradient-to-b from-[#60ab2c] from-20% to-[#9c643f] to-40% h-screen">
     <div class="mx-auto h-screen bg-[url('/bg.jpg')] bg-cover">
       <div class="backdrop-blur-3xl h-full w-full p-8 flex items-center justify-center">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center bg-none relative">
           <div class="absolute top-[-18rem] left-[-5rem] right-[-5rem]">
-            <img src="/sga_mc_cropped.png" class="mx-auto"></img>
+            <img src="/sga_mc_cropped.png" class="mx-auto drop-shadow-[0_3px_3px_black]"></img>
           </div>
           <img
             src="/sga_mc_background.png"
-            class="absolute top-[100%] translate-y-[-50%] -z-10 min-w-[35rem]"
+            class="absolute top-[100%] translate-y-[-50%] -z-10 min-w-[35rem] drop-shadow-[0_3px_3px_black]"
           >
           </img>
 
